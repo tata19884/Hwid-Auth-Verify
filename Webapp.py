73e616ee679899310d633922edce1b9ae327fbe7
_______ <53409082+xannyyyy@users.noreply.github.com>
2020-02-06 18:57:16 +0100

    Create Webapp.py

---
+++ Webapp.py
@@ -0,0 +1,44 @@
+import logging, json
+
+from flask import Flask, render_template, request
+
+log = logging.getLogger('werkzeug')
+app = Flask('')
+hwidsList = []
+log.setLevel(logging.ERROR)
+
+with open ("Auth/hwid.txt", "r") as f:
+  hwids = f.read()
+hwidsSplitted = hwids.split("\n")
+for i in range(0, len(hwidsSplitted)):
+  hwidsList.append(hwidsSplitted[i])
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/backend', methods=['GET'])
+def test():
+    hwid = request.args.get('hwid')
+    
+    true = {
+     'whitelisted': True,
+     'status': 200
+    }
+
+    false = {
+     'whitelisted': False,
+     'status': 401
+    }
+
+    if hwid not in hwids:
+      return json.dumps(false), 401
+    else:
+      return json.dumps(true), 200
+
+@app.errorhandler(404) 
+def not_found(e): 
+    return render_template("NotFoundError.html"), 404
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
