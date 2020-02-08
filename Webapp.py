5453396f26f06922544788a868cbcb3042b2f4ad
_______ <53409082+xannyyyy@users.noreply.github.com>
2020-02-08 18:40:35 +0100

    Update Webapp.py

--- Webapp.py
+++ Webapp.py
@@ -41,4 +41,4 @@ def not_found(e):
     return render_template("NotFoundError.html"), 404
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="localhost", port=8080)
